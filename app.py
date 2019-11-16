ad2a419be74cab1e958b7abbdf69b27ca0cd114e
Phong Lam <lamnd@chainos.vn>
2019-11-16 13:33:43 +0700

    update app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 myclient = MongoClient("mongodb://localhost:27017/")
 
 db = myclient["Website"]
-col = db["Admin"]
 print("sucess")
 
 @app.route('/loginmanage', methods=['GET', 'POST'])
 def loginmanage():
+    col = db["Admin"]
     error = None
     if request.method == 'POST':
     	print(request.form['username'])
@@ -25,6 +25,33 @@ def loginmanage():
 def index():
   return render_template('index.html')
 
+@app.route('/shop', methods=['GET', 'POST'])
+def shop():
+  return render_template('shop.html')
+
+@app.route('/single_product', methods=['GET', 'POST'])
+def single_product():
+  return render_template('single_product.html')
+
+@app.route('/payment', methods=['GET', 'POST'])
+def payment():
+  return render_template('payment.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  return render_template('login.html')
+
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+  return render_template('about.html')
+
+@app.route('/checkout', methods=['GET', 'POST'])
+def checkout():
+  return render_template('checkout.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def login():
+  return render_template('contact.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001)
