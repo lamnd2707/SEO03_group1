4fb1ea6f0ac32dfe970f8553f149d0ed5a3afa2c
Nguyễn Anh Tú <nguyenanhtu_t62@hus.edu.vn>
2019-12-16 13:56:01 +0700

    update img

--- app.py
+++ app.py
@@ -118,4 +118,4 @@ def test():
     return render_template('Test.html', a=a, sug = sug)
 
 if __name__ == "__main__":
-    app.run(host ='0.0.0.0', port = 8080, debug=True)
+    app.run( debug=True)
